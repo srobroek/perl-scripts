dad8a71a9de3cd76cc95a705b4ef59ae27f78c84
Sjors Robroek <s.robroek@itq.nl>
2014-06-16 15:32:06 +0200

    no message

--- SjorsR/removeVMfromDRSGroup.pl
+++ SjorsR/removeVMfromDRSGroup.pl
@@ -62,7 +62,7 @@ print join("\n", @$groupvms);
 print $vm_view->{'mo_ref'};
 @$groupvms = grep { $_ != $vm_view}  @$groupvms;
 print join("\n", @$groupvms);
-print "\n".$vm_view->{'mo_ref'};
+print "\nVMVIEW:".$vm_view->{'mo_ref'};
 
 
 $groupSpec = new ClusterGroupSpec();
