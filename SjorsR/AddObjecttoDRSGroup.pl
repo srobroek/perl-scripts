b12f4261f600ae9f292388851c57e8dafe89d39e
Sjors Robroek <s.robroek@itq.nl>
2014-06-16 11:53:42 +0200

    no message

--- SjorsR/AddObjecttoDRSGroup.pl
+++ SjorsR/AddObjecttoDRSGroup.pl
@@ -66,14 +66,14 @@ die "Failed to find virtual machine DRS group '$drsgroup_name'" unless $drsgroup
 $groupobjects = eval { $drsgroup->{'object'} } || [ ];
 push @$groupobjects, $object_view->{'mo_ref'};
 
-my $groupSpec = new ClusterGroupSpec();
-#$groupSpec->{'operation'} = new ArrayUpdateOperation("edit");
-#$groupSpec->{'info'}->{'vm'} = [ @$groupobjects ];
-my $clusterSpec = new ClusterConfigSpecEx();
+$groupSpec = new ClusterGroupSpec();
+$groupSpec->{'operation'} = new ArrayUpdateOperation("edit");
+$groupSpec->{'info'}->{'vm'} = [ @$groupobjects ];
+$clusterSpec = new ClusterConfigSpecEx();
 $clusterSpec->{'groupSpec'} = [ $groupSpec ];
-print $clusterSpec;
-print $cluster_view;
-print @$groupobjects;
+#print $clusterSpec;
+#print $cluster_view;
+#print @$groupobjects;
 $cluster_view->ReconfigureComputeResource(spec => $clusterSpec, modify => 1);
 
 Util::disconnect();
